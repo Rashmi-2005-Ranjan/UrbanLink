d3cd9542783c9b07d7629c575272e3c21a98609c
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-03-19 00:50:23 +0530

    Configure Routing Files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,27 @@
 import { Routes } from '@angular/router';
 import { SearchComponent } from './search/search.component';
 import { LoginComponent } from './Auth Components/login/login.component';
+import { RegisterComponent } from './Auth Components/register/register.component';
+import { ServiceListComponent } from './service-list/service-list.component';
+import { BookingComponent } from './booking/booking.component';
+import { authGuard } from './auth.guard';
+import { MyBookingsComponent } from './my-bookings/my-bookings.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
-    {path:'',component:SearchComponent},
-    {path:'login',component:LoginComponent}
+  { path: '', component: SearchComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'services', component: ServiceListComponent },
+  {
+    path: 'booking/:id',
+    component: BookingComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'my-bookings',
+    component: MyBookingsComponent,
+    canActivate: [authGuard],
+  },
+  {path:'**',component:NotFoundComponent}
 ];
