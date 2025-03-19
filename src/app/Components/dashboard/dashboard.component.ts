38b2c7b29ed4008e8548059effe7fa81be3f979f
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-03-19 19:24:40 +0530

    Designed Dashboard Content and side bar component

--- src/app/Components/dashboard/dashboard.component.ts
+++ src/app/Components/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SideBarComponent } from '../side-bar/side-bar.component';
+import { DashboardContentComponent } from '../dashboard-content/dashboard-content.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterModule],
+  imports: [RouterModule,SideBarComponent,DashboardContentComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
