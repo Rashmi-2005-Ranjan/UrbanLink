40a1591c6cec759b226e28165f54b30db8847204
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-03-21 17:00:57 +0530

    Implemented Logout Functionality In User Dashboard

--- src/app/Components/user-dashboard/sidebar/sidebar.component.ts
+++ src/app/Components/user-dashboard/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AuthServicesService } from '../../../Services/auth-services.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,5 +9,8 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-
+  private auth=inject(AuthServicesService)
+logout():void{
+  this.auth.logout();
+}
 }
