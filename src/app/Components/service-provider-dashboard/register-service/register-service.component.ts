97d52bbdf79f23b2e62d9298e9e664d8342c1d95
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-03-21 18:06:41 +0530

    Created Component For Service Provider

---
+++ src/app/Components/service-provider-dashboard/register-service/register-service.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-service',
+  imports: [],
+  templateUrl: './register-service.component.html',
+  styleUrl: './register-service.component.css'
+})
+export class RegisterServiceComponent {
+
+}
