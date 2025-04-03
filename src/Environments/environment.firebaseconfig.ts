c15139763c74d4b4e04078ca264e470d4f6412cf
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-04-03 07:28:18 +0530

    Add Firebase Environment

---
+++ src/Environments/environment.firebaseconfig.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDVzynxiUKafbzuogjrxg-Cwe9yNQ6JJYA',
+    authDomain: 'urbanlink-aab2c.firebaseapp.com',
+    projectId: 'urbanlink-aab2c',
+    storageBucket: 'urbanlink-aab2c.firebasestorage.app',
+    messagingSenderId: '128441792415',
+    appId: '1:128441792415:web:f2c0223f6c470d3a33efaa',
+    measurementId: 'G-4ZPC7Q98GN',
+  },
+};
