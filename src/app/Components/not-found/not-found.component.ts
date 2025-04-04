1277522d9528025b1e00fc531fc38940bcee7f07
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-04-04 18:44:40 +0530

    Implemented Not Found Component Wit Wild Card Routing

--- src/app/Components/not-found/not-found.component.ts
+++ src/app/Components/not-found/not-found.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.css'
+  styleUrl: './not-found.component.css',
 })
 export class NotFoundComponent {
+  showAnimation = false;
+  showOptions = false;
+  hoverState = '';
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.showAnimation = true;
+    }, 300);
+  }
+
+  toggleOptions() {
+    this.showOptions = !this.showOptions;
+  }
+
+  navigateTo(path: string) {
+    this.hoverState = path;
+    setTimeout(() => {
+      this.router.navigate([path]);
+    }, 300);
+  }
 }
